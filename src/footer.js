3a67fb836e74fbe31f6a82b9cebd7ef7ae6f99da
MBP27 <MBP27@mac.home>
2025-09-04 15:07:01 +0200

    add image manipulation based on which page

--- src/footer.js
+++ src/footer.js
@@ -3,6 +3,31 @@ document.addEventListener("DOMContentLoaded", function () {
 		.then((response) => response.text())
 		.then((data) => {
 			document.querySelector("footer").innerHTML = data;
+			setFooterImage();
 		})
 		.catch((error) => console.error("Error loading footer:", error));
 });
+
+// Function to set image source based on the current page
+function setFooterImage() {
+	const currentPage = window.location.pathname;
+	const acceuilImage = document.getElementById("accueilPicto");
+	const carteImage = document.getElementById("cartePicto");
+	const participantsImage = document.getElementById("participantsPicto");
+	const programmeImage = document.getElementById("programmePicto");
+
+	if (currentPage.includes("index.html"))
+		return acceuilImage.classList
+			.add("selected")
+			.classList.remove("notSelected");
+	if (currentPage.includes("carte.html"))
+		return carteImage.classList.add("selected").classList.remove("notSelected");
+	if (currentPage.includes("participants.html"))
+		return participantsImage.classList
+			.add("selected")
+			.classList.remove("notSelected");
+	if (currentPage.includes("progamme.html"))
+		return programmeImage.classList
+			.add("selected")
+			.classList.remove("notSelected");
+}
