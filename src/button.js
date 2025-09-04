7e6627ee9915c0d42afef0afa8c9fd8e80732b90
MBP27 <MBP27@mac.home>
2025-09-04 17:37:22 +0200

    make the button interactive

---
+++ src/button.js
@@ -0,0 +1,14 @@
+const carteButton = document.getElementById("carteButton");
+const participantsButton = document.getElementById("participantsButton");
+const programmeButton = document.getElementById("programmeButton");
+
+carteButton.addEventListener("click", function () {
+	window.location.href = "carte.html";
+});
+
+participantsButton.addEventListener("click", function () {
+	window.location.href = "participants.html";
+});
+programmeButton.addEventListener("click", function () {
+	window.location.href = "progamme.html";
+});
