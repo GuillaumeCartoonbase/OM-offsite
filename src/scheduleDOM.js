a7724716a2e0d49a89097b125f86e0a17de00369
MBP27 <MBP27@mac.home>
2025-09-17 09:27:59 +0200

    remove bloat comment

--- src/scheduleDOM.js
+++ src/scheduleDOM.js
@@ -11,12 +11,6 @@ fetch("src/schedule.json")
 	.catch((error) => console.error("Error loading the JSON file:", error));
 
 function displaySchedule() {
-	// (Optional) clear previous content if re-rendering:
-	// ["thursday","friday","saturday","sunday"].forEach(id => {
-	//   const el = document.getElementById(id);
-	//   if (el) el.innerHTML = "";
-	// });
-
 	scheduleData.forEach((orga) => {
 		const li = document.createElement("li");
 		li.innerHTML = `
