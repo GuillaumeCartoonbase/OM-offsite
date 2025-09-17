1304b77110041fa5734aae1c3b0674d9f569dcc7
MBP27 <MBP27@mac.home>
2025-09-17 10:34:45 +0200

    add list of link if multiples

--- src/scheduleDOM.js
+++ src/scheduleDOM.js
@@ -12,14 +12,23 @@ fetch("src/schedule.json")
 
 function displaySchedule() {
 	scheduleData.forEach((orga) => {
+		let sublist = "";
+		if (orga.link.length > 1) {
+			for (let i = 0; i < orga.link.length; i++) {
+				sublist += `<li class="subset"> <a href = ${orga.link[i][1]}> ${orga.link[i][0]} </a> </li>`;
+			}
+		}
+
 		const li = document.createElement("li");
 		li.classList.add("timeline");
 		li.innerHTML = `
       <h3 class="schedule">${orga.time}</h3>
 	       <h4 class="schedTitle">${
-						orga.link ? "<a href=" + orga.link + ">" : ""
+						orga.link.length == 1 ? "<a href=" + orga.link[0][1] + ">" : ""
 					} ${orga.titre} ${orga.link ? "</a>" : ""}</h4>
       <p class="schedText">${orga.text}</p>
+	  ${orga.link.length > 1 ? "<ul>" + sublist + "</ul>" : ""}
+	  
     `;
 
 		const dayContainer = document.getElementById(orga.day); // expects "thursday" | "friday" | ...
