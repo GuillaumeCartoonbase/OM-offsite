02d8c47adeeb5301669df9c14ac11711b926dde1
MBP27 <MBP27@mac.home>
2025-09-19 14:13:17 +0200

    add setup for password

---
+++ src/protection.js
@@ -0,0 +1,22 @@
+const SITE_PASSWORD = "Fedora2025";
+function grantAccess() {
+	localStorage.setItem("site_access", "granted");
+	document.body.style.display = "block"; // show content
+}
+function denyAccess() {
+	document.body.innerHTML = "Accès refusé. Actualisez pour retenter.";
+	// document.body.style.display = "block"; // show message
+}
+window.addEventListener("load", () => {
+	if (localStorage.getItem("site_access") === "granted") {
+		// Already unlocked
+		document.body.style.display = "block";
+	} else {
+		const entered = prompt("Entrez le mot de passe");
+		if (entered === SITE_PASSWORD) {
+			grantAccess();
+		} else {
+			denyAccess();
+		}
+	}
+});
