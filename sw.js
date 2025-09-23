610fd601404a209d59c51867b2741e0089165526
MBP27 <MBP27@mac.home>
2025-09-23 14:28:47 +0200

    change cache version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE_NAME = "app-cache-v3";
+const CACHE_NAME = "app-cache-v4";
 const PRECACHE = [
 	"/", // optional if your root HTML rarely changes
 	"/offline.html", // <-- make sure this file exists
